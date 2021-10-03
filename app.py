fe29fcb186ed22cb843cb4d5932dc8ac287d41f8
Sasetz <56696451a@gmail.com>
2021-10-03 20:35:50 +0300

    Test commit

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from core import *
 from bottle import run
 import os
 
+print("test")
+
 if __name__ == '__main__':
     if os.environ.get('APP_LOCATION') == 'heroku':
         run(host=setup.base_url, port=int(os.environ.get("PORT", 5000)))
